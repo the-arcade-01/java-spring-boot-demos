452ff54ecf2e0220b533fd9224c469b57acd450c
the-arcade-01 <aashisharcade@gmail.com>
2022-07-28 16:45:34 +0530

    elasticsearch project added

---
+++ elasticsearch/src/main/java/com/arcade/elasticsearch/document/Person.java
@@ -0,0 +1,40 @@
+package com.arcade.elasticsearch.document;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+@Document(indexName = "person")
+@Setting(settingPath = "static/es-settings.json")
+public class Person {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
