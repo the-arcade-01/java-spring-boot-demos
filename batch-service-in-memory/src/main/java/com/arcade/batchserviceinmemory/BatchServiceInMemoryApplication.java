a9c6a644e908ce7e8b0a016dc75df4e67ff36dd2
the-arcade-01 <aashisharcade@gmail.com>
2022-08-28 23:51:56 +0530

    batch-service in-memory storage added

---
+++ batch-service-in-memory/src/main/java/com/arcade/batchserviceinmemory/BatchServiceInMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.arcade.batchserviceinmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServiceInMemoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServiceInMemoryApplication.class, args);
+	}
+
+}
---
+++ batch-service-in-memory/src/test/java/com/arcade/batchserviceinmemory/BatchServiceInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcade.batchserviceinmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchServiceInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
