dc556719ce37f8c03e70b38ddd5a2c18d9a88630
the-arcade-01 <aashisharcade@gmail.com>
2022-08-28 22:32:46 +0530

    batch service 2 added

---
+++ batch-service2/src/main/java/com/arcade/batchservice2/BatchService2Application.java
@@ -0,0 +1,13 @@
+package com.arcade.batchservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchService2Application.class, args);
+	}
+
+}
---
+++ batch-service2/src/test/java/com/arcade/batchservice2/BatchService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arcade.batchservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
